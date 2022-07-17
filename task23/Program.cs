634f1048264208d5784d7cdf716dee514b5285ce
AnnA <anna_elfimova@mail.ru>
2022-07-17 11:22:35 +0300

    task 19 / palindrom

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+ while (count <= N)
+{
+  Console.WriteLine(Math.Pow(count,3));
+  count ++;
+}
